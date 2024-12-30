e6c295d8be87dc4c054939dc7abc850d0513ac25
NK28116 <shoutarou.phillip.w@gmail.com>
2024-12-30 19:32:45 +0900

    それぞれ独立したCounterを作成

--- win-rate-react19/src/Counter.tsx
+++ win-rate-react19/src/Counter.tsx
@@ -1,33 +1,42 @@
-import { useActionState } from "react";
+import { useActionState,useState } from "react";
 
 
 async function increment(previousState:number): Promise<number> {
   return previousState + 1;
 }
 
+function useIncrmentState({}) {
+    const [state, formAction] = useActionState(increment, 0);
+    return (
+
+        <form>
+       {state}
+        <button formAction={formAction}>stateIncrement</button>
+        </form>
+    )
+}
+
 async function decrement(previousState:number): Promise<number> {
   return previousState - 1;
 }
 
-function ObjectName() {
-    return<label>相手キャラ: <input type="text" placeholder="これがプレースホルダー"/></label>
+function useDecrmentState({}) {
+    const [state, formAction] = useActionState(decrement, 0);
+    return (
+        <form>
+        {state}
+        <button formAction={formAction}>stateDecrement</button>
+        </form>
+    )
 }
 
-function CountRate({ rate }: { rate: number }) {
-    return <div>WinRate: {rate.toFixed(2)}</div>;
-}
+
 export function CounterApp() {
-  const [stateWin, formAction] = useActionState(increment, 0);
-  const [stateLose, formAction2] = useActionState(decrement, 0);
 
-  return (
-
-    <form>
-    <ObjectName/>
-      {stateWin}
-      <button formAction={formAction}>statewinIncrement</button>
-      {stateLose}
-      <button formAction={formAction2}>stateloseDecrement</button>
-    </form>
-  )
+    return (
+        <>
+            {useIncrmentState({})}
+            {useDecrmentState({})}
+        </>
+    )
 }
