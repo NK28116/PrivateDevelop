3111357a4018141b594d0ca634ecfb43212c1e22
NK28116 <161926307+NK28116@users.noreply.github.com>
2025-05-25 22:55:51 +0900

    Delete pythonVScodeTest directory

--- pythonVScodeTest/VScodeTestPython.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-data=pd.read_csv('../Downloads/NUMBERS3_ALL.csv')
-
-for col in data.columns:
-    data[col].value_counts().sort_index.plot(kind='bar',title=f"Digit Distribution in {col}")
-    plt.show()
